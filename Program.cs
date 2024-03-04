8897f734ef0df8bc0d0af2a1edacc06e772a6a92
Rahul Khanal <rahul39khanal@gmail.com>
2024-03-04 13:33:38 +0545

    type casting error solved

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MyConsoleApp.entities;
+
+List<Game> games = new(){
+    new Game(){
+        Id = 18,
+        Name= "Stree fighter",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "https://placehold.co/100"
+    },
+     new Game(){
+        Id = 10,
+        Name= "PUBG",
+        Genre = "Fighter",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "https://placehold.co/100"
+    },
+     new Game(){
+        Id = 1,
+        Name= "Clash of Clan",
+        Genre = "Strategies",
+        Price = 90.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "https://placehold.co/100"
+    },
+     new Game(){
+        Id = 2,
+        Name= "UEFA football",
+        Genre = "Sport",
+        Price = 7.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "https://placehold.co/100"
+    },
+};
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World");
+app.MapGet("/games", () => games);
 
 app.Run();
--- entities/Game.cs
+++ entities/Game.cs
@@ -1,13 +1,14 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-
 namespace MyConsoleApp.entities
 {
     public class Game
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? MyProperty { get; set; }
+        public string? ImageUri { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
